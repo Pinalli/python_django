34e2a2ebe83314088a963f9c68eec98a4c767058
Alberto Rocha Pinalli <betopinalli@gmail.com>
2020-07-07 20:57:37 -0300

    criado o front end

--- gerenciador_tarefas/app/views.py
+++ gerenciador_tarefas/app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .forms import TarefaForm
 
 
 def listar_tarefas(request):
     nome_tarefa = "Estudar Python e Django"
     return render(request, 'tarefas/listar_tarefas.html', {"nome_tarefa": nome_tarefa})
+
+
+def cadastrar_tarefa(request):
+    form_tarefa = TarefaForm()
+    return render(request, 'tarefas/form_tarefa.html', {"form_tarefa": form_tarefa})
